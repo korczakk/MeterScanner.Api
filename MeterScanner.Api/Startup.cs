5bdbaff20de50a4c7b07fcb20e8918e7cac6414f
kamil.korczak <kamil.korczak@gmail.com>
2020-09-06 21:24:56 +0200

    Setting DB

--- MeterScanner.Api/Startup.cs
+++ MeterScanner.Api/Startup.cs
@@ -11,6 +11,9 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 
+using MeterScanner.Infrastructure.Database;
+using Microsoft.EntityFrameworkCore;
+
 namespace MeterScanner.Api
 {
   public class Startup
@@ -25,6 +28,10 @@ namespace MeterScanner.Api
     // This method gets called by the runtime. Use this method to add services to the container.
     public void ConfigureServices(IServiceCollection services)
     {
+      services.AddDbContext<MeterScannerDbContext>(options =>
+      {
+        options.UseSqlServer(Configuration.GetConnectionString("Default"));
+      });
       services.AddControllers();
     }
 
---
+++ MeterScanner.Infrastructure/Database/MeterScannerDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace MeterScanner.Infrastructure.Database
+{
+  public class MeterScannerDbContext : IdentityDbContext
+  {
+    public MeterScannerDbContext(DbContextOptions options)
+      : base(options)
+    {
+    }
+  }
+}
